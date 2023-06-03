8fbfe1e80c9198d891a9c73a5da7c119017d0fbf
kevasesk <kevasqwerty@gmail.com>
2023-06-03 15:26:28 +0300

    init front.

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+      <h1>H1 Poppins light 40</h1>
+      <h2>H1 Poppins light 36</h2>
+      <h3>H1 Poppins light 36</h3>
+  </main>
+</template>
+
+<style scoped>
+</style>
